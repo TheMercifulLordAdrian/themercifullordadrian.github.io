b7ae0220482aaa49d592f1e8407f4dca521254ef
TheMercifulLordAdrian <adriancar@live.com>
2015-11-05 15:34:20 +0800

    kjwfheq

--- game/js/game.js
+++ game/js/game.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
 
   $("#start").click(function() {
     $("#start").hide();
+    $("#titleText").hide();
+    $("#makerCredits").hide();
+    $("#lordAdrian").hide();
+    $("#dialogue1").css('display', 'block');
   });
 
 });
